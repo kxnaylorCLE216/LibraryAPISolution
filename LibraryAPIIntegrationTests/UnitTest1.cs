1470d9afd601b2f58f1b6ea2c16cb804436ff7bf
KYLE X NAYLOR <naylorkx@gmail.com>
2020-10-27 17:51:07 +0000

    after lunch

---
+++ LibraryAPIIntegrationTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LibraryAPIIntegrationTests
+{
+   public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int a = 10, b = 20, answer;
+
+            answer = a + b;
+
+            Assert.Equal(30, answer);
+
+        }
+    }
+}
